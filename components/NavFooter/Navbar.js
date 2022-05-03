90c9d94f4ede2489f1e370ed89458a4da960f114
krishnabharambe <krishnabharambe@gmail.com>
2022-05-03 08:37:11 +0530

    added Navbar

--- components/NavFooter/Navbar.js
+++ components/NavFooter/Navbar.js
@@ -1,145 +1,145 @@
-import { Fragment, useState } from 'react';
-import { Dialog, Popover, Tab, Transition } from '@headlessui/react';
+import { Fragment, useState } from "react";
+import { Dialog, Popover, Tab, Transition } from "@headlessui/react";
 import {
   MenuIcon,
   SearchIcon,
   ShoppingBagIcon,
   XIcon,
-} from '@heroicons/react/outline';
-import Link from 'next/link';
+} from "@heroicons/react/outline";
+import Link from "next/link";
 
 const navigation = {
   categories: [
     {
-      id: 'women',
-      name: 'Women',
+      id: "women",
+      name: "Women",
       featured: [
         {
-          name: 'New Arrivals',
-          href: '#',
+          name: "New Arrivals",
+          href: "#",
           imageSrc:
-            'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
+            "https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg",
           imageAlt:
-            'Models sitting back to back, wearing Basic Tee in black and bone.',
+            "Models sitting back to back, wearing Basic Tee in black and bone.",
         },
         {
-          name: 'Basic Tees',
-          href: '#',
+          name: "Basic Tees",
+          href: "#",
           imageSrc:
-            'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
+            "https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg",
           imageAlt:
-            'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
+            "Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.",
         },
       ],
       sections: [
         {
-          id: 'clothing',
-          name: 'Clothing',
+          id: "clothing",
+          name: "Clothing",
           items: [
-            { name: 'Tops', href: '#' },
-            { name: 'Dresses', href: '#' },
-            { name: 'Pants', href: '#' },
-            { name: 'Denim', href: '#' },
-            { name: 'Sweaters', href: '#' },
-            { name: 'T-Shirts', href: '#' },
-            { name: 'Jackets', href: '#' },
-            { name: 'Activewear', href: '#' },
-            { name: 'Browse All', href: '#' },
+            { name: "Tops", href: "#" },
+            { name: "Dresses", href: "#" },
+            { name: "Pants", href: "#" },
+            { name: "Denim", href: "#" },
+            { name: "Sweaters", href: "#" },
+            { name: "T-Shirts", href: "#" },
+            { name: "Jackets", href: "#" },
+            { name: "Activewear", href: "#" },
+            { name: "Browse All", href: "#" },
           ],
         },
         {
-          id: 'accessories',
-          name: 'Accessories',
+          id: "accessories",
+          name: "Accessories",
           items: [
-            { name: 'Watches', href: '#' },
-            { name: 'Wallets', href: '#' },
-            { name: 'Bags', href: '#' },
-            { name: 'Sunglasses', href: '#' },
-            { name: 'Hats', href: '#' },
-            { name: 'Belts', href: '#' },
+            { name: "Watches", href: "#" },
+            { name: "Wallets", href: "#" },
+            { name: "Bags", href: "#" },
+            { name: "Sunglasses", href: "#" },
+            { name: "Hats", href: "#" },
+            { name: "Belts", href: "#" },
           ],
         },
         {
-          id: 'brands',
-          name: 'Brands',
+          id: "brands",
+          name: "Brands",
           items: [
-            { name: 'Full Nelson', href: '#' },
-            { name: 'My Way', href: '#' },
-            { name: 'Re-Arranged', href: '#' },
-            { name: 'Counterfeit', href: '#' },
-            { name: 'Significant Other', href: '#' },
+            { name: "Full Nelson", href: "#" },
+            { name: "My Way", href: "#" },
+            { name: "Re-Arranged", href: "#" },
+            { name: "Counterfeit", href: "#" },
+            { name: "Significant Other", href: "#" },
           ],
         },
       ],
     },
     {
-      id: 'men',
-      name: 'Men',
+      id: "men",
+      name: "Men",
       featured: [
         {
-          name: 'New Arrivals',
-          href: '#',
+          name: "New Arrivals",
+          href: "#",
           imageSrc:
-            'https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg',
+            "https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg",
           imageAlt:
-            'Drawstring top with elastic loop closure and textured interior padding.',
+            "Drawstring top with elastic loop closure and textured interior padding.",
         },
         {
-          name: 'Artwork Tees',
-          href: '#',
+          name: "Artwork Tees",
+          href: "#",
           imageSrc:
-            'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg',
+            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg",
           imageAlt:
-            'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
+            "Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.",
         },
       ],
       sections: [
         {
-          id: 'clothing',
-          name: 'Clothing',
+          id: "clothing",
+          name: "Clothing",
           items: [
-            { name: 'Tops', href: '#' },
-            { name: 'Pants', href: '#' },
-            { name: 'Sweaters', href: '#' },
-            { name: 'T-Shirts', href: '#' },
-            { name: 'Jackets', href: '#' },
-            { name: 'Activewear', href: '#' },
-            { name: 'Browse All', href: '#' },
+            { name: "Tops", href: "#" },
+            { name: "Pants", href: "#" },
+            { name: "Sweaters", href: "#" },
+            { name: "T-Shirts", href: "#" },
+            { name: "Jackets", href: "#" },
+            { name: "Activewear", href: "#" },
+            { name: "Browse All", href: "#" },
           ],
         },
         {
-          id: 'accessories',
-          name: 'Accessories',
+          id: "accessories",
+          name: "Accessories",
           items: [
-            { name: 'Watches', href: '#' },
-            { name: 'Wallets', href: '#' },
-            { name: 'Bags', href: '#' },
-            { name: 'Sunglasses', href: '#' },
-            { name: 'Hats', href: '#' },
-            { name: 'Belts', href: '#' },
+            { name: "Watches", href: "#" },
+            { name: "Wallets", href: "#" },
+            { name: "Bags", href: "#" },
+            { name: "Sunglasses", href: "#" },
+            { name: "Hats", href: "#" },
+            { name: "Belts", href: "#" },
           ],
         },
         {
-          id: 'brands',
-          name: 'Brands',
+          id: "brands",
+          name: "Brands",
           items: [
-            { name: 'Re-Arranged', href: '#' },
-            { name: 'Counterfeit', href: '#' },
-            { name: 'Full Nelson', href: '#' },
-            { name: 'My Way', href: '#' },
+            { name: "Re-Arranged", href: "#" },
+            { name: "Counterfeit", href: "#" },
+            { name: "Full Nelson", href: "#" },
+            { name: "My Way", href: "#" },
           ],
         },
       ],
     },
   ],
   pages: [
-    { name: 'Company', href: '#' },
-    { name: 'Stores', href: '#' },
+    { name: "Company", href: "#" },
+    { name: "Stores", href: "#" },
   ],
 };
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Navbar() {
@@ -197,9 +197,9 @@ export default function Navbar() {
                         className={({ selected }) =>
                           classNames(
                             selected
-                              ? 'text-indigo-600 border-indigo-600'
-                              : 'text-gray-900 border-transparent',
-                            'flex-1 whitespace-nowrap py-4 px-1 border-b-2 text-base font-medium'
+                              ? "text-indigo-600 border-indigo-600"
+                              : "text-gray-900 border-transparent",
+                            "flex-1 whitespace-nowrap py-4 px-1 border-b-2 text-base font-medium"
                           )
                         }
                       >
@@ -231,11 +231,13 @@ export default function Navbar() {
                               href={item.href}
                               className="mt-6 block font-medium text-gray-900"
                             >
-                              <span
-                                className="absolute z-10 inset-0"
-                                aria-hidden="true"
-                              />
-                              {item.name}
+                              <>
+                                <span
+                                  className="absolute z-10 inset-0"
+                                  aria-hidden="true"
+                                />
+                                {item.name}
+                              </>
                             </Link>
                             <p aria-hidden="true" className="mt-1">
                               Shop now
@@ -298,19 +300,7 @@ export default function Navbar() {
                 </div>
               </div>
 
-              {/* <div className="border-t border-gray-200 py-6 px-4">
-                <Link href="#" className="-m-2 p-2 flex items-center">
-                  <img
-                    src="https://tailwindui.com/img/flags/flag-canada.svg"
-                    alt=""
-                    className="w-5 h-auto block flex-shrink-0"
-                  />
-                  <span className="ml-3 block text-base font-medium text-gray-900">
-                    CAD
-                  </span>
-                  <span className="sr-only">, change currency</span>
-                </Link>
-              </div> */}
+          
             </div>
           </Transition.Child>
         </Dialog>
@@ -338,13 +328,13 @@ export default function Navbar() {
 
               {/* Logo */}
               <div className="ml-4 flex lg:ml-0">
-                <Link href="#">
-                  <span className="sr-only">Workflow</span>
+                <Link href="##">
+                  <><span className="sr-only">Workflow</span>
                   <img
                     className="h-8 w-auto"
                     src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
                     alt=""
-                  />
+                  /></>
                 </Link>
               </div>
 
@@ -359,9 +349,9 @@ export default function Navbar() {
                             <Popover.Button
                               className={classNames(
                                 open
-                                  ? 'border-indigo-600 text-indigo-600'
-                                  : 'border-transparent text-gray-700 hover:text-gray-800',
-                                'relative z-10 flex items-center transition-colors ease-out duration-200 text-sm font-medium border-b-2 -mb-px pt-px'
+                                  ? "border-indigo-600 text-indigo-600"
+                                  : "border-transparent text-gray-700 hover:text-gray-800",
+                                "relative z-10 flex items-center transition-colors ease-out duration-200 text-sm font-medium border-b-2 -mb-px pt-px"
                               )}
                             >
                               {category.name}
@@ -403,12 +393,12 @@ export default function Navbar() {
                                           <Link
                                             href={item.href}
                                             className="mt-6 block font-medium text-gray-900"
-                                          >
+                                          ><>
                                             <span
                                               className="absolute z-10 inset-0"
                                               aria-hidden="true"
                                             />
-                                            {item.name}
+                                            {item.name}</>
                                           </Link>
                                           <p
                                             aria-hidden="true"
@@ -490,16 +480,14 @@ export default function Navbar() {
 
                 {/* Cart */}
                 <div className="ml-4 flow-root lg:ml-6">
-                  <Link href="#" className="group -m-2 p-2 flex items-center">
-                    <ShoppingBagIcon
+                  <Link href="####" className="group -m-2 p-2 flex items-center">
+                    <div><ShoppingBagIcon
                       className="flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-500"
                       aria-hidden="true"
-                    />
-                    <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+                    /><span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
                       0
-                    </span>
-                    <span className="sr-only">items in cart, view bag</span>
-                  </Link>
+                    </span><span className="sr-only">items in cart, view bag</span>
+                </div></Link>
                 </div>
               </div>
             </div>
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,125 @@
-export default function Home() {
-  return <div>hello</div>;
+import { signOut, useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { getCookies } from 'cookies-next';
+import { useRouter } from 'next/router';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateUserInfo } from '../store/usersSlice';
+import Layout from '../components/Layout';
+import Slider1 from '../components/HomePage/Slider1';
+import LoadingComp from '../components/loadingComp';
+import MainServicesView from '../components/HomePage/mainServicesView';
+
+export default function Home({ data2, MainServices }) {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const [isLoading, SetIsLoading] = useState(false);
+  const [imgsliderData, setImgSliderData] = useState(null);
+  const dispatch = useDispatch();
+  console.log('MainServices', MainServices);
+
+  // const images = [
+  //   'https://krishnabharambe.pythonanywhere.com/media/SliderImages/shop_banner_43in_x_84in_01_starrr_1.png',
+  //   'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=60',
+  //   'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=60',
+  //   'https://images.unsplash.com/photo-1458668383970-8ddd3927deed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=60',
+  // ];
+
+  let images = [];
+  const getSliderdata = () => {
+    images = [];
+    for (let i = 0; i < data2.length; i++) {
+      images.push(data2[i].image);
+    }
+
+    setImgSliderData(images);
+  };
+
+  useEffect(() => {
+    SetIsLoading(true);
+    const token = getCookies('token');
+    // getsliderImageDetails();
+    if (!token.token) {
+      signOut({ callbackUrl: '/auth' });
+    }
+
+    if (!session) {
+      signOut({ callbackUrl: '/auth' });
+    }
+
+    if (token.token && status === 'authenticated') {
+      axios
+        .get('https://krishnabharambe.pythonanywhere.com/api/userAPI/', {
+          headers: {
+            Authorization: `token ${token.token}`,
+          },
+        })
+        .then((res) => {
+          if (session.id == token.token) {
+            dispatch(
+              updateUserInfo({
+                id: res.data.id,
+                phone: res.data.phone,
+                first_login: res.data.first_login,
+              })
+            );
+            SetIsLoading(false);
+            getSliderdata();
+          } else {
+            //redirect to login
+            SetIsLoading(false);
+            signOut({ callbackUrl: '/auth' });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          SetIsLoading(false);
+          signOut({ callbackUrl: '/auth' });
+        });
+    }
+  }, [status]);
+
+  return (
+    <>
+      {!isLoading ? (
+        <>
+          <div>
+            {session ? (
+              <>
+                <Layout>
+                  <div className="py-2">
+                    <Slider1 data2={imgsliderData} />
+                    <MainServicesView data2={MainServices} />
+                  </div>
+                  {/* <h1 className="">Session - {session.id}</h1>
+                  <h1 className="">Status-- {status}</h1> */}
+                </Layout>
+              </>
+            ) : (
+              'not authenticated'
+            )}
+          </div>
+        </>
+      ) : (
+        <LoadingComp />
+      )}
+    </>
+  );
 }
+
+export async function getStaticProps() {
+  const data2 = await axios.get(
+    'https://krishnabharambe.pythonanywhere.com/api/allSlidercards'
+  );
+
+  const MainServices = await axios.get(
+    'https://krishnabharambe.pythonanywhere.com/api/MainServicesList/'
+  );
+  return {
+    props: {
+      data2: data2.data ?? {},
+      MainServices: MainServices.data ?? {},
+    },
+  };
+}
+Home.auth = true;
